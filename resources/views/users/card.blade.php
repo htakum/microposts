faf89471d3a01534962beb090add12048bbf83b8
takumi.harada3 <takumi.h240@gmail.com>
2023-06-15 01:45:28 +0000

    follow, unfollow

--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -1,3 +1,5 @@
+@extends('user_follow.follow_button')
+
 <div class="card border border-base-300">
     <div class="card-body bg-base-200 text-4xl">
         <h2 class="card-title">{{ $user->name }}</h2>
@@ -7,3 +9,5 @@
         <img src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
     </figure>
 </div>
+{{-- フォロー／アンフォローボタン --}}
+@include('user_follow.follow_button')
